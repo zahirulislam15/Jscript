b81c7f454d2d82f6cc01c856809522f7fd7b3708
zahirulislam15 <zahirulislam15@gmail.com>
2021-04-21 02:07:26 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,7 @@
+let x, z;       // Declare Variables
+x = 5; 
+const y = 6.22;      // Assign Values
+z = x + y;
+
+document.getElementById("demo").innerHTML = "ramadanul mubarak "+y;
+document.getElementById("demo1").innerHTML = "ramadanul mubarak "+z;
