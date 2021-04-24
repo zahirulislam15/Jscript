f80380822f245dfe6d980d7f51140e8517e44059
zahirulislam15 <zahirulislam15@gmail.com>
2021-04-24 20:00:19 +0600

    function

--- hello.js
+++ hello.js
@@ -1,7 +1,21 @@
-let x, z;       // Declare Variables
+/*let x, z;       // Declare Variables
 x = 5; 
 const y = 6.22;      // Assign Values
 z = x + y;
 
-document.getElementById("demo").innerHTML = "ramadanul mubarak "+y;
-document.getElementById("demo1").innerHTML = "ramadanul mubarak "+z;
+//document.getElementById("demo").innerHTML = "ramadanul mubarak "+y;
+//document.getElementById("demo1").innerHTML = "ramadanul mubarak "+z;
+
+//console.log(z);
+
+//Boolean 
+console.log(typeof 7);
+var a=false;
+if(a)console.log("true");
+else console.log("false");
+*/
+
+
+//function
+ a=(a,b)=>{console.log(a+b)}
+a(7,1);
